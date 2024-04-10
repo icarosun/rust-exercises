30a823d6f94e33ad5ff99d7364c06c027c0cf362
Ícaro <icaropereirasun@gmail.com>
2024-04-09 23:12:20 -0400

    feature: add first test case

--- fizz_buzz/src/main.rs
+++ fizz_buzz/src/main.rs
@@ -11,17 +11,27 @@ fn main() {
 
     let num: i32 = scan_num.trim().parse().expect("A number");
 
-    fizz_buzz(num);
+    println!("{}", fizz_buzz(num));
 }
 
-fn fizz_buzz(x: i32){
+fn fizz_buzz(x: i32) -> String {
     if x % 5 == 0 && x % 3 == 0 {
-        println!("FizzBuzz");
+        return "FizzBuzz".to_string();
     } else if x % 5 == 0 {
-        println!("Buzz");
+        return "Buzz".to_string();
     } else if x % 3 == 0 {
-        println!("Fizz");
+        return "Fizz".to_string();
     } else {
-        println!("{x}");
+        return x.to_string();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fizz() {
+        assert_eq!("Fizz", fizz_buzz(3));
     }
 }
